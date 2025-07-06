5193a7526595fb34b062665534f01251d0bdc9b1
vssundararaman <vssundararaman@gmail.com>
2025-07-06 00:41:51 +0000

    07052025

---
+++ Algorithm/ReverseNumber.java
@@ -0,0 +1,22 @@
+/**
+ * Reverse the number
+ */
+public class ReverseNumber{
+    public static void main(String[] args) {
+        System.out.println(reverseNumber(1234));
+    }
+
+    static int reverseNumber(int num) {
+        int result = 0;
+   
+        int sign = num<0 ?-1: 1;
+        num = Math.abs(num);
+
+        while(num!=0){
+            int digit = num % 10;
+            result = result * 10 + digit;
+            num = num/10;
+        }
+        return sign * result;
+    }
+}
